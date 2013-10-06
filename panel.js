4ae985e94f352ae95b5a2dd41a0c5081003f1c92
Justin Tadlock <justin@justintadlock.com>
2013-10-05 21:53:11 -0500

    Completely recode, well, pretty much the entire thing.

--- panel.js
+++ panel.js
@@ -1,14 +1,32 @@
-var $j = jQuery.noConflict();
-$j(document).ready(function () {
-	$j('#sliding-panel .open').click(function () {
-		$j('#sliding-panel .panel').slideDown('slow');
-		$j('#sliding-panel .tab').addClass('current')
-	});
-	$j('#sliding-panel .close').click(function () {
-		$j('#sliding-panel .panel').slideUp('slow');
-		$j('#sliding-panel .tab').removeClass('current')
-	});
-	$j('#sliding-panel .toggle a').click(function () {
-		$j('#sliding-panel .toggle a').toggle()
-	})
-});
+jQuery( document ).ready(
+	function () {
+
+		jQuery( '.show-if-js' ).show();
+
+		jQuery( '#sliding-panel .sp-toggle a' ).text( sp_l10n.open );
+		jQuery( '#sliding-panel .sp-toggle a' ).attr( 'aria-selected', 'false' );
+
+		jQuery( '#sliding-panel .sp-toggle a' ).click(
+			function( j ) {
+
+				j.preventDefault();
+
+				jQuery( this ).parents( '#sliding-panel' ).find( '.sidebar' ).slideToggle(
+					'slow',
+					function() {
+
+						if ( jQuery( this ).is( ':visible' ) ) {
+							jQuery( '#sliding-panel .sp-toggle a' ).text( sp_l10n.close );
+							jQuery( '#sliding-panel .sp-toggle a' ).attr( 'aria-selected', 'true' );
+						}
+
+						else {
+							jQuery( '#sliding-panel .sp-toggle a' ).text( sp_l10n.open );
+							jQuery( '#sliding-panel .sp-toggle a' ).attr( 'aria-selected', 'false' );
+						}
+					}
+				);
+			}
+		);
+	}
+);
--- sliding-panel.php
+++ sliding-panel.php
@@ -7,134 +7,208 @@
  * Author: Justin Tadlock
  * Author URI: http://justintadlock.com
  *
- * Creates a sliding panel for use on any WordPress site. This has been tested
- * and auto-inserts itself for a few themes.  Other themes need to manually
- * call the sliding panel within the content.
- * @link http://themehybrid.com/themes/hybrid
- * @link http://themeshaper.com/thematic
- *
- * The original idea for this plugin came from Web Kreation, but the plugin has been 
- * coded from the ground up to work with many themes, handle widgets, and work 
- * along with the version of jQuery packaged with WordPress.
- * @link http://web-kreation.com/index.php/wordpress/implement-a-nice-clean-jquery-sliding-panel-in-wordpress-27
- *
  * @copyright 2009
  * @version 0.2.0
  * @author Justin Tadlock
  * @link http://justintadlock.com/archives/2009/06/25/sliding-panel-wordpress-plugin
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
  * @package SlidingPanel
  */
 
-/**
- * Yes, we're localizing the plugin.  This partly makes sure non-English
- * users can use it too.  To translate into your language use the
- * en_EN.po file as as guide.  Poedit is a good tool to for translating.
- * @link http://poedit.net
- *
- * @since 0.1
- */
-load_plugin_textdomain( 'sliding-panel', false, '/sliding-panel' );
+final class Sliding_Panel_Plugin {
+
+	/**
+	 * Holds the instance of this class.
+	 *
+	 * @since  0.2.0
+	 * @access private
+	 * @var    object
+	 */
+	private static $instance;
+
+	/**
+	 * Stores the directory path for this plugin.
+	 *
+	 * @since  0.2.0
+	 * @access private
+	 * @var    string
+	 */
+	private $directory_path;
+
+	/**
+	 * Stores the directory URI for this plugin.
+	 *
+	 * @since  0.2.0
+	 * @access private
+	 * @var    string
+	 */
+	private $directory_uri;
+
+	/**
+	 * Plugin setup.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+
+		/* Set the properties needed by the plugin. */
+		add_action( 'plugins_loaded', array( $this, 'setup' ), 1 );
+
+		/* Internationalize the text strings used. */
+		add_action( 'plugins_loaded', array( $this, 'i18n' ), 2 );
+
+		/* Register sidebars late so ours come after theme sidebars. */
+		add_action( 'widgets_init', array( $this, 'register_sidebars' ), 95 );
+
+		/* Load scripts and styles. */
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 5 );
+
+		/* Load the sliding panel sidebar in the footer. */
+		add_action( 'wp_footer', array( $this, 'sliding_panel' ), 0 );
+	}
+
+	/**
+	 * Defines the directory path and URI for the plugin.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function setup() {
+
+		$this->directory_path = trailingslashit( plugin_dir_path( __FILE__ ) );
+		$this->directory_uri  = trailingslashit( plugin_dir_url(  __FILE__ ) );
+	}
+
+	/**
+	 * Loads the translation files.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function i18n() {
+
+		/* Load the translation of the plugin. */
+		load_plugin_textdomain( 'sliding-panel', false, 'sliding-panel/languages' );
+	}
+
+	/**
+	 * Loads the admin functions and files.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function admin() {
+
+	//	if ( is_admin() )
+	//		require_once( "{$this->directory_path}admin/admin.php" );
+	}
+
+	/**
+	 * Registers sidebars.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function register_sidebars() {
+
+		register_sidebar( 
+			array(
+				'id'            => 'sliding-panel',
+				'name'          => __( 'Sliding Panel', 'sliding-panel' ),
+				'before_widget' => '<section id="%1$s" class="widget %2$s widget-%2$s">',
+				'after_widget'  => '</section>',
+				'before_title'  => '<h3 class="widget-title">',
+				'after_title'   => '</h3>'
+			)
+		);
+	}
+
+	/**
+	 * Loads scripts and styles.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function enqueue_scripts() {
+
+		if ( is_active_sidebar( 'sliding-panel' ) ) {
+
+			wp_register_script( 'sliding-panel', "{$this->directory_uri}panel.js", array( 'jquery' ), '0.1', true );
+
+			wp_localize_script(
+				'sliding-panel',
+				'sp_l10n',
+				array(
+					'open'  => __( 'Open',  'sliding-panel' ),
+					'close' => __( 'Close', 'sliding-panel' )
+				)
+			);
+
+			wp_enqueue_script( 'sliding-panel' );
+
+			wp_enqueue_style( 'sliding-panel', "{$this->directory_uri}panel.css", false, 0.1, 'screen' );
+		}
+	}
+
+	/**
+	 * Loads the sliding panel sidebar.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return void
+	 */
+	public function sliding_panel() {
+		require_once( "{$this->directory_path}inc/sidebar-sliding-panel.php" );
+	}
+
+	/**
+	 * Returns the instance.
+	 *
+	 * @since  0.2.0
+	 * @access public
+	 * @return object
+	 */
+	public static function get_instance() {
+
+		if ( !self::$instance )
+			self::$instance = new self;
+
+		return self::$instance;
+	}
+}
+
+Sliding_Panel_Plugin::get_instance();
 
 /**
- * Make sure we get the correct directory.
- * @since 0.1
- */
-if ( !defined('WP_CONTENT_URL' ) )
-	define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
-if ( !defined( 'WP_CONTENT_DIR' ) )
-	define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
-if ( !defined( 'WP_PLUGIN_URL' ) )
-	define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
-if ( !defined( 'WP_PLUGIN_DIR' ) )
-	define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
-
-/**
- * Define constant paths to the plugin folder.
- * @since 0.1
- */
-define( SLIDING_PANEL, WP_PLUGIN_DIR . '/sliding-panel' );
-define( SLIDING_PANEL_URL, WP_PLUGIN_URL . '/sliding-panel' );
-
-/**
- * Add actions
- * @since 0.1
- */
-add_action( 'init', 'sliding_panel_register_sidebars' );
-add_action( 'template_redirect', 'load_sliding_panel' );
-
-/**
- * Loads JS and CSS if the sidebar is active and not in admin.
- * @since 0.1
- */
-if ( !is_admin() && is_active_sidebar( 'sliding-panel' ) ) :
-	wp_enqueue_style( 'sliding-panel', SLIDING_PANEL_URL . '/panel.css', false, 0.1, 'screen' );
-	wp_enqueue_script( 'sliding-panel-js', SLIDING_PANEL_URL . '/panel.js', array( 'jquery' ), '0.1', true );
-endif;
-
-/**
- * Registers the sliding panel widget area.
- * @uses register_sidebar()
- *
- * @since 0.1
+ * @since      0.1.0
+ * @deprecated 0.2.0
  */
 function sliding_panel_register_sidebars() {
-	register_sidebar( array( 'name' => __('Sliding Panel', 'sliding-panel'), 'id' => 'sliding-panel', 'before_widget' => '<div id="%1$s" class="widget %2$s widget-%2$s"><div class="widget-inside">', 'after_widget' => '</div></div>', 'before_title' => '<h3 class="widget-title">', 'after_title' => '</h3>' ) );
+	_deprecated_function( __FUNCTION__, '0.2.0', 'Sliding_Panel_Plugin::register_sidebars()' );
 }
 
 /**
- * Hooks the sliding panel into one of the available action hooks.
- * This works with the Hybrid, Thematic, and Prodigy theme.
- * @uses hybrid_before_html()
- * @uses thematic_before()
- * @uses prodigy_before()
- *
- * @since 0.1
+ * @since      0.1.0
+ * @deprecated 0.2.0
  */
 function load_sliding_panel() {
-	if ( is_admin() )
-		return '';
-
-	add_action( 'hybrid_before_html', 'get_sliding_panel' );
-	add_action( 'thematic_before', 'get_sliding_panel' );
-	add_action( 'prodigy_before', 'get_sliding_panel' );
+	_deprecated_function( __FUNCTION__, '0.2.0', '' );
 }
 
 /**
- * Displays the sliding panel if the widget area is active.
- * This can be manually called in the templates with:
- *	<?php if ( function_exists( 'sliding_panel' ) ) sliding_panel(); ?>
- *
- * @uses is_active_sidebar()
- * @since 0.1
+ * @since      0.1.0
+ * @deprecated 0.2.0
  */
 function get_sliding_panel() {
-
-	if ( is_active_sidebar( 'sliding-panel' ) ) : ?>
-
-	<div id="sliding-panel-container"><div id="sliding-panel">
-
-		<div class="panel">
-			<div class="panel-content">
-				<?php dynamic_sidebar( 'sliding-panel' ); ?>
-			</div>
-		</div>
-
-		<div class="tab">
-			<div class="toggle">
-				<a class="open" title="<?php _e('Open panel', 'sliding-panel'); ?>"><?php _e('Open <span class="arrow">&darr;</span>', 'sliding-panel'); ?></a>
-				<a class="close" title="<?php _e('Close panel', 'sliding-panel'); ?>" style="display: none;"><?php _e('Close <span class="arrow">&uarr;</span>', 'sliding-panel'); ?></a>
-			</div>
-		</div>
-
-	</div></div>
-
-	<?php endif;
+	_deprecated_function( __FUNCTION__, '0.2.0', 'Sliding_Panel_Plugin::sliding_panel()' );
 }
 
 ?>
